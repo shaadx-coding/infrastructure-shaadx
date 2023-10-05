7c2e886e4780d055c55c8ff04c23b1add94f8fc0
Shaadx <shaadx@protonmail.com>
2023-10-05 23:40:30 +0200

    terraform: vault: alias: Fixed typo in backend name

--- terraform/vault/alias.tf
+++ terraform/vault/alias.tf
@@ -1,5 +1,5 @@
 resource "vault_identity_entity_alias" "shaadx" {
   name = vault_identity_entity.shaadx.name
-  mount_accessor = vault_auth_backed.userpass.accessor
+  mount_accessor = vault_auth_backend.userpass.accessor
   canonical_id   = vault_identity_entity.shaadx.id
 }
