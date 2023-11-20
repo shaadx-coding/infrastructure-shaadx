53f5e9a7ab7d728fe1d25b07675b06757dd05a6f
Shaadx <shaadx@protonmail.com>
2023-11-20 23:20:47 +0100

    terraform: secrets: argocd: Change cluster path

--- terraform/secrets/argocd.tf
+++ terraform/secrets/argocd.tf
@@ -3,8 +3,8 @@
 #
 
 resource "vault_generic_secret" "k8s-in-cluster_argocd_secret" {
-  path = "k8s-in-cluster/argocd/secret"
+  path = "in-cluster/argocd/secret"
   data_json = jsonencode({
-    ARGOCD_ADMIN_PASSWORD = "FIX ME"
+    ARGOCD_ADMIN_PASSWORD = "FIX-ME"
   })
 }
